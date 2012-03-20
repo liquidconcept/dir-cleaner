40d37130daa37c0e9e9d08f49c6fd06a38f3c228
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-03-20 16:13:09 +0100

    add delete action

--- clean.rb
+++ clean.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'trollop'
+require 'pathname'
 
 # Pasrsing options
 options = Trollop::options do
@@ -8,7 +9,7 @@ options = Trollop::options do
 Clean share folder
 
 Usage:
-       clean [options] <dirnames>+
+       clean [options] <dirnames>
 
 where [options] are:
 EOS
@@ -22,18 +23,24 @@ Trollop::die 'need at least one dirname' if folders.empty?
 
 # 
 now = Time.now.to_i / 60 / 60 / 24 # Now in days from unixtime
-Dir.glob(folders.map {|dir| "#{dir}/**/*" }, File::FNM_DOTMATCH) do |item|
-  if item !~ /^(?:.+\/)?\.{1,2}$/ # regex test if ending by /. or /.. 
-
-    if !File.directory?(item)
-      item_days = File.stat(item).atime.to_i / 60 / 60 / 24
-      if (now - item_days) >= 4 
-        puts "delete file '#{item}'"
-        # delete item
+Dir.glob(folders.map {|dir| "#{dir}/**/*" }, File::FNM_DOTMATCH) do |path|
+  if path !~ /^(?:.+\/)?\.{1,2}$/ # regex test if ending by /. or /.. 
+    path = Pathname.new(path)
+    # 
+    puts path  
+    if !path.directory?
+      path_days = path.atime.to_i / 60 / 60 / 24
+      # 
+      puts "path_days => " + path_days.to_s
+      puts "now => " + now.to_s
+      if (now - path_days) >= options[:days]
+        puts "delete file '#{path}'"
+        path.delete
       end
     else
-      if Dir.entries(item).select {|entry| entry !~ /^\.\.?$/ }.empty?
-        puts "delete dir '#{item}'"
+      if path.entries.select {|entry| entry.to_s !~ /^\.\.?$/ }.empty?
+        puts "delete dir '#{path}'"
+        path.delete
       end
     end
   end
