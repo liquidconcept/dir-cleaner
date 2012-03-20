47ce86637493cf75b4bd59959f18c54a25ad968f
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-03-20 11:25:27 +0100

    Pasrsing options and add

--- clean.rb
+++ clean.rb
@@ -1,20 +1,40 @@
-#Avec chemin
-#Dir.glob("*", File::FNM_DOTMATCH) {|filename| puts File.expand_path(filename) }
+#!/usr/bin/env ruby
+require 'trollop'
 
-#sans le chemin
-# directories_file = Dir.glob("*", File::FNM_DOTMATCH)
-# directories_file.each do |item|
-#   if item != "." && item != ".."
-#     puts item
-#   end
-# end
+# Pasrsing options
+options = Trollop::options do
+  version 'clean 0.1.0 (c) 2012 Liquid Concept'
+  banner <<-EOS
+Clean share folder
 
-Dir.glob("*", File::FNM_DOTMATCH) do |item|
+Usage:
+       clean [options] <dirnames>+
+
+where [options] are:
+EOS
+
+  opt :days, 'Time ago to keep files in days', :default => 30
+end
+folders = ARGV.map {|dir| File.expand_path(dir) }.select {|dir| File.directory?(dir) }
+
+# Validation of options
+Trollop::die 'need at least one dirname' if folders.empty?
+
+# 
+now = Time.now.to_i / 60 / 60 / 24 # Now in days from unixtime
+Dir.glob(folders.map {|dir| "#{dir}/**/*" }, File::FNM_DOTMATCH) do |item|
   if item !~ /^(?:.+\/)?\.{1,2}$/ # regex test if ending by /. or /.. 
-    puts item + " => " + File.stat(item).atime.to_s
-    t = Time.new.yday # return nb day of the year
-    t_item = File.stat(item).atime.yday
-    lapstime = t - t_item
-    puts "time => " + lapstime.to_s
+
+    if !File.directory?(item)
+      item_days = File.stat(item).atime.to_i / 60 / 60 / 24
+      if (now - item_days) >= 4 
+        puts "delete file '#{item}'"
+        # delete item
+      end
+    else
+      if Dir.entries(item).select {|entry| entry !~ /^\.\.?$/ }.empty?
+        puts "delete dir '#{item}'"
+      end
+    end
   end
 end
