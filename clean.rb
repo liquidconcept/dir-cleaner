7e166568184b9a5269d1ca5cb7c6de83d25c7867
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2012-03-22 14:07:25 +0100

    add dry's option to specified the delete action

--- clean.rb
+++ clean.rb
@@ -9,38 +9,37 @@ options = Trollop::options do
 Clean share folder
 
 Usage:
-       clean [options] <dirnames>
+       clean [options] <dirnames> <day> <dry>
 
 where [options] are:
 EOS
 
   opt :days, 'Time ago to keep files in days', :default => 30
+  opt :dry, 'Condition to delete files and folders', :default => false
 end
 folders = ARGV.map {|dir| File.expand_path(dir) }.select {|dir| File.directory?(dir) }
 
 # Validation of options
-Trollop::die 'need at least one dirname' if folders.empty?
+Trollop::die 'need at least one path' if folders.empty?
 
-# 
 now = Time.now.to_i / 60 / 60 / 24 # Now in days from unixtime
 Dir.glob(folders.map {|dir| "#{dir}/**/*" }, File::FNM_DOTMATCH) do |path|
   if path !~ /^(?:.+\/)?\.{1,2}$/ # regex test if ending by /. or /.. 
     path = Pathname.new(path)
-    # 
-    puts path  
     if !path.directory?
       path_days = path.atime.to_i / 60 / 60 / 24
-      # 
-      puts "path_days => " + path_days.to_s
-      puts "now => " + now.to_s
       if (now - path_days) >= options[:days]
-        puts "delete file '#{path}'"
-        path.delete
+        puts "[FILE] delete '#{path}'"
+        if options[:dry] == true
+          path.delete
+        end
       end
     else
       if path.entries.select {|entry| entry.to_s !~ /^\.\.?$/ }.empty?
-        puts "delete dir '#{path}'"
-        path.delete
+        puts "[DIR] delete '#{path}'"
+        if options[:dry] == true
+          path.delete
+        end
       end
     end
   end
